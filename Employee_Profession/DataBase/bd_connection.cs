c3fe8a60e2f9e60934f5d046618135b856ffab27
Зиля Шагиахметова <zilyasagiahmetova03@mail.ru>
2025-01-11 20:49:21 +0300

    Добавьте файлы проекта.

---
+++ Employee_Profession/DataBase/bd_connection.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Employee_Profession.DataBase
+{
+    public class bd_connection
+    {
+        public static Employee_ProfesionEntities connection = new Employee_ProfesionEntities();
+    }
+}
